2bad2e3f145d857843276fd8edc30a1e175d0037
kumarswamy2025 <156901978+kumarswamy2025@users.noreply.github.com>
2025-07-19 13:06:47 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (76.67%) - LeetSync

---
+++ Fast_and_Slow_Pointers/Easy/141-linked-list-cycle/linked-list-cycle.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    // flow and fast pointer
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head; // slow pointer start at begging node
+        ListNode fast=head; // fast pointer start at begging node
+
+        while(fast!=null && fast.next!=null ){
+            // slow pointer move one step forward
+            slow=slow.next;
+            // fast pointer move two steps forward
+            fast=fast.next.next;
+
+            // checking if fast and slow pointers are meet at point
+            if(slow==fast){  
+                return true; //cycle exists
+            }
+        
+        }
+        return false; // cycle does not exits
+
+
+    }
+
+    // using hashset
+    static boolean hashsetApporach(ListNode head) {
+
+        HashSet<ListNode> set = new HashSet<>();
+
+        ListNode temp = head;
+        while (temp != null) {
+            // if ele is already avalable in set if there then return true
+            if (set.contains(temp)) {
+                return true;
+            }
+            // if it is not present then add it to set
+            else {
+                set.add(temp);
+                temp = temp.next;
+            }
+        }
+        // if there is no cycle then return false
+        return false;
+    }
+}
