515a3edf8972ed00e82a8eb2f64e822632663c4f
kumarswamy2025 <156901978+kumarswamy2025@users.noreply.github.com>
2025-07-19 13:23:21 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (95.05%) - LeetSync

---
+++ Fast_and_Slow_Pointers/Easy/908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    // using slow and fast pointer
+    public ListNode middleNode(ListNode head) {
+         ListNode slow=head; // slow pointer start at begging node
+        ListNode fast=head; // fast pointer start at begging node
+
+       while(fast!=null && fast.next!=null ){
+            // slow pointer move one step forward
+            slow=slow.next;
+            // fast pointer move two steps forward
+            fast=fast.next.next;
+
+            
+        
+        }
+        
+         return slow;   // middle ele
+    }
+
+    // brute force apporach :  traverse linked list and count elements and divide by 2 we get middle element 
+    static ListNode bruteForce(ListNode head) {
+        ListNode temp=head;
+        int count=0;
+        while(temp!=null){
+            count++;
+            temp=temp.next;
+
+        }
+        temp=head;
+        // traverse head upto count/2 times
+        for(int i=0;i<count/2;i++){
+       
+       temp=temp.next;
+     
+        }
+
+        return temp;
+    }
+
+}
